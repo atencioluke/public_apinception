3c34f958f90688b20e9623c89109e887d27fed56
atencioluke <atencioluke@gmail.com>
2020-09-15 23:47:39 -0700

    Added randomized exit screens, updated selection and instruction format and text.

--- lib/public_apinception/cli.rb
+++ lib/public_apinception/cli.rb
@@ -4,9 +4,11 @@ class PublicApinception::CLI
     def initialize
         t1 = Thread.new do
             @ascii = PublicApinception::ASCIIIMAGES.new
+            system "printf '\e[8;50;130t'"
+            
             ascii.load_screen
         end
-
+        
         t2 = Thread.new do
             @prompt = TTY::Prompt.new
             PublicApinception::Adapter.new
@@ -21,62 +23,114 @@ class PublicApinception::CLI
     end
 
     def welcome
+        # system "printf '\e[8;48;130t'"
         @ascii.welcome_screen
         puts "Hi! Welcome to Public APInception! What kind of API are you looking for today?"
         list_categories
     end
 
-    def loading
-    end
-
     def list_categories
-        # PublicApinception::Category.all.map {|c| c.category }
         input = list(@categories)
 
-        if input == "Exit"
-            exit
-        else
-            list_apis(input)
-        end
+        input == "Exit" ? exit : list_apis(input)
     end
 
     def list_apis(input)
+        system "clear"
+        puts ascii.title_screen
         apis = PublicApinception::API.title_by_category(input)
-        apis << "Go back to categories"
+        apis << "Go back to Categories"
         choice = list(apis)
 
         if choice == "Exit"
             exit
-        elsif choice == "Go back to categories"
+        elsif choice == "Go back to Categories"
             list_categories
         else
-            api_info(choice)
+            api_info(choice, input)
         end
     end
     
-    def api_info(input)
+    def api_info(input, previous_choice)
+        # header
+        system "clear"
         info = PublicApinception::API.find_by_title(input)
 
-        binding.pry
         selection = <<~HERE
-        Title: 
-        Description: 
-        Auth_type: 
-        HTTPS: 
-        Cors: 
-        Link: 
-        Category: 
+        +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+                      Title
+        +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+
+        #{info.title}
+
+        +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+                    Description
+        +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+
+        #{info.description}
+
+        +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+                     Details
+        +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+
+        Auth_type:   #{info.auth_type == "" ? "N/A" : info.auth_type }
+        HTTPS:       #{info.https}
+        Cors:        #{info.cors}
+        Link:        #{info.link}
+        Category:    #{info.category}
+
         HERE
-        
+
+        puts selection
+        toggle = end_menu(previous_choice)
+
+        if toggle == "Exit"
+            exit
+        elsif toggle == "Go back to Categories"
+            list_categories
+        else
+            list_apis(toggle)
+        end
+
+    end
+
+    def end_menu(other_options)
+        options = ["Go back to Categories", other_options]
+
+        input = list(options)
+    end
+
+    def header
+        system "clear"
+        puts ascii.title_screen
     end
 
     def list(options)
-        options << "Exit"
-        input = prompt.select("Select an option using the arrow keys and press enter, and letter keys to filter. (↑/↓ navigate options, ←/→ navigate pages)", options, cycle: true, per_page: 10, filter: true)
+        directions = <<~HERE
+        
+        +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+        
+        ███████╗███████╗██╗     ███████╗ ██████╗████████╗     █████╗ ███╗   ██╗     ██████╗ ██████╗ ████████╗██╗ ██████╗ ███╗   ██╗   
+        ██╔════╝██╔════╝██║     ██╔════╝██╔════╝╚══██╔══╝    ██╔══██╗████╗  ██║    ██╔═══██╗██╔══██╗╚══██╔══╝██║██╔═══██╗████╗  ██║██╗
+        ███████╗█████╗  ██║     █████╗  ██║        ██║       ███████║██╔██╗ ██║    ██║   ██║██████╔╝   ██║   ██║██║   ██║██╔██╗ ██║╚═╝
+        ╚════██║██╔══╝  ██║     ██╔══╝  ██║        ██║       ██╔══██║██║╚██╗██║    ██║   ██║██╔═══╝    ██║   ██║██║   ██║██║╚██╗██║██╗
+        ███████║███████╗███████╗███████╗╚██████╗   ██║       ██║  ██║██║ ╚████║    ╚██████╔╝██║        ██║   ██║╚██████╔╝██║ ╚████║╚═╝
+        ╚══════╝╚══════╝╚══════╝╚══════╝ ╚═════╝   ╚═╝       ╚═╝  ╚═╝╚═╝  ╚═══╝     ╚═════╝ ╚═╝        ╚═╝   ╚═╝ ╚═════╝ ╚═╝  ╚═══╝   
+                                                                                                                                      
+        Use the ⬆/⬇ keys to navigate options, ⬅/⮕ keys to navigate pages
+        You can filter by typing what you want. (E.g.Typing "Cat" will give you Cats & Cat Facts)
+        Press enter to confirm your selection.
+        +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+        HERE
+
+        options.include?("Exit") || options << "Exit"
+        input = prompt.select(directions, options, cycle: true, per_page: 10, filter: true)
     end
 
     def exit
-        ascii.load_screen
-        puts "It's been real, it's been good, but it hasn't been real good."
+        system "clear"
+        ascii.exit_screen
+        sleep(3)
+        system "clear"
     end
 end
